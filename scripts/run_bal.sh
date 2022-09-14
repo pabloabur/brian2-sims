b2c22a4a67a68c45e87c13b021deeebf8973cfd5
pabloabur <pabloabur@gmail.com>
2022-09-14 18:45:04 +1000

    Updates script that runs balanced network (more samples)

--- scripts/run_bal.sh
+++ scripts/run_bal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 folder_name="sim_data/balance_exp"
 mkdir -p $folder_name && echo "Balanced network with minifloat." >> ${folder_name}/description.txt
-for wi in {90..109}; do
+for wi in {70..115}; do
     for trial in {0..9}; do
         sim_name=$(date +"${folder_name}/%d-%m_%Hh%Mm%Ss")
         python simulations/SLIF_balanced_tutorial.py --wi $wi --trial $trial --path ${sim_name}/ --quiet
