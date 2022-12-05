07977f7bfe632816e00c1019806f9ab7dddfd8e6
pabloabur <pabloabur@gmail.com>
2022-12-05 11:56:07 +1100

    WIP Adjusts R script to generate plots

--- plots/PD_activity.R
+++ plots/PD_activity.R
@@ -2,63 +2,98 @@ library(ggplot2)
 library(tidyr)
 library(stringr)
 library(dplyr)
+library(arrow)
+library(purrr)
 
-# TODO sample before saving to disk?
-a=arrow::read_feather('spikes.feather')
-# TODO this needs to be loaded?
-tsim <- 0.1 # TODO 60k-400/k for prot 1; something else for 2
-# TODO both below also depend on protocol
-min_time <- 0
+spikes <- read_feather('spikes.feather')
+
+tsim <- 60 # TODO needs adjusting for full simulations
+
+# Raster plot, protocol 1; somethig like .4s duration
+min_time <- 59600
 max_time <- tsim*1000
-# Approximately 20 per layer
-# TODO sample proportion? I could do it when saving to disk and in here use proportion again
-# NB:
-# psample is used to get % of each layer for rates
-# rates and cv taken from dataframe containing only nsamples from each layer
-# sync from nsampled dataframe, so it's like sampling it likewise too
-# AIness is just a result taken from each folder
-# for protocol2, psample taken from each layer
-n_sample <- 80
-
-hist_bins <- seq(0, tsim*1000+3, by=3)
-
-df_spk <- a %>%
-    #slice_sample(n=n_sample) %>%
+p_sample <- 0.025
+df_raster <- spikes %>%
+    group_by(layer) %>%
+    slice_sample(prop=p_sample) %>%
     unnest_longer(t) %>%
     mutate(laminae=str_sub(layer, 1, -2)) %>%
     # In case we want to change order
     #mutate(laminae=fct_relevel(laminae, c("L4", "L23", "L5", "L6"))) %>%
     filter(t>=min_time & t<=max_time)
 
-rates <- a %>%
-    rowwise() %>%
-    mutate(rate=length(t)/tsim) %>%
+# Raster plot, protocol 2; interval can be selected
+min_time <- 600
+max_time <- 1000
+df_raster2 <- spikes %>%
     group_by(layer) %>%
-    summarise(rate=mean(rate))
+    slice_sample(prop=p_sample) %>%
+    unnest_longer(t) %>%
+    mutate(laminae=str_sub(layer, 1, -2)) %>%
+    filter(t>=min_time & t<=max_time)
 
-# R uses unbiased estimation, so result will probably be different from numpy
-cv <- a %>%
-    mutate(isi=map(t, diff))%>%
-    rowwise()%>%
-    mutate(cv=sd(isi)/mean(isi)) %>%
-    group_by(layer) %>%
-    summarise(cv=mean(cv))
-
-# TODO first 500ms are ignored, that is why [166:]
-sync_var <- df_spk %>% group_by(layer) %>% summarise(sync_var=var(hist(t, hist_bins, plot=F, right=F)$counts))
-sync_mean <- df_spk %>% group_by(layer) %>% summarise(sync_mean=mean(hist(t, hist_bins, plot=F, right=F)$counts))
-syncs <- full_join(sync_mean, sync_var, by="layer") %>%
-    rowwise() %>%
-    mutate(sync=sync_var/sync_mean)
-
-# TODO protocol2; np.sum(np.split(count, tsim), axis=0) for each layer; the rest is similar
-hist_bins2 <- seq(0, tsim*1000+.5, by=.5)
-
-spks <- ggplot(df_spk, aes(x=t, y=i, color=layer)) +
-
+# TODO colored by exc/inh for each layer
+# TODO remove ylabel, ticks, and adjust xlabel (size and name and in s)
+spks <- ggplot(df_raster, aes(x=t, y=i, color=layer)) +
          geom_point() + theme_bw() +
          theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank()) +
          facet_grid(rows=vars(laminae), scales="free")
 
+# Measures
+n_sample <- 1000
+df_measures <- spikes %>%
+    group_by(layer) %>%
+    slice_sample(n=n_sample)
+
+rates <- df_measures %>%
+    rowwise() %>%
+    mutate(rate=length(t)/tsim)
+
+# R uses unbiased estimation, so result will be slightly different from numpy
+cv <- df_measures %>%
+    mutate(isi=map(t, diff))%>%
+    rowwise()%>%
+    mutate(cv=sd(isi, na.rm=T)/mean(isi, na.rm=T))
+
+hist_bins <- seq(0, tsim*1000+3, by=3)
+sync_var <- df_measures %>%
+    unnest_longer(t) %>%
+    # Removing initial transients
+    filter(t>=500) %>%
+    group_by(layer) %>%
+    summarise(sync_var=var(hist(t, hist_bins, plot=F, right=F)$counts))
+sync_mean <- df_measures %>%
+    unnest_longer(t) %>%
+    # Removing initial transients
+    filter(t>=500) %>%
+    group_by(layer) %>%
+    summarise(sync_mean=mean(hist(t, hist_bins, plot=F, right=F)$counts, na.rm=T))
+syncs <- full_join(sync_mean, sync_var, by="layer") %>%
+    rowwise() %>%
+    mutate(sync=sync_var/sync_mean)
+
+# AI landscape
+# TODO AIness taken from multiple folders
+# sys glob *.feather; files <- map(files, read_feather); map(gh_repos, ~map_dbl(.x, ~.x[["size"]])) %>% map(~max(.x))
+avg_rates <- rates %>%
+    group_by(layer) %>%
+    summarise(rate=mean(rate, na.rm=T))
+avg_cv <- cv %>%
+    group_by(layer) %>%
+    summarise(cv=mean(cv, na.rm=T))
+df_measures <- reduce(list(avg_rates, avg_cv, select(syncs, layer, sync)), left_join, by="layer")
+df_measures <- df_measures %>%
+    mutate(ai=if_else(rate<30 & sync<=8 & cv>=0.7 & cv<=1.2, T, F))
+ainess = 100*sum(df_measures$ai)/8.0
+
+# TODO Styles for plots here
 freq <- ggplot(rates, aes(rate, layer)) +
     geom_boxplot()
+irregularity <- ggplot(df_measures, aes(cv, layer)) +
+    geom_col()
+synchronicity <- ggplot(df_measures, aes(sync, layer)) +
+    geom_col()
+#ggsave('test.png', freq)
+
+# TODO protocol2; np.sum(np.split(count, tsim), axis=0) for each layer; the rest is similar
+hist_bins2 <- seq(0, tsim*1000+.5, by=.5)
