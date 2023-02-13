14af3dc11557556d175e3760d7244557f4de5741
pabloabur <pabloabur@gmail.com>
2023-02-13 17:55:55 +1100

    Adds fixes and improvements to stdp tests

--- core/equations/synapses/fp8STDP.py
+++ core/equations/synapses/fp8STDP.py
@@ -4,17 +4,23 @@ from core.equations.synapses.fp8CUBA import fp8CUBA, ParamDict
 class fp8STDP(fp8CUBA):
     def __init__(self):
         super().__init__()
-        self.model += 'w_plast : volt\n'
-        self.modify_model('model', '', old_expr='weight : volt')
+        self.model += 'w_plast : integer\n'
+        self.modify_model('model', '', old_expr='weight : integer')
 
         self.modify_model('on_pre',
                           'fp8_multiply(w_plast, w_factor)',
                           old_expr='fp8_multiply(weight, w_factor)')
-        self.on_pre += 'w_plast = clip(w_plast - eta*Ca_post, 0, inf)\n'
+        self.on_pre += ('weighted_Ca = fp8_multiply(eta, Ca_post)\n'
+                        + 'weighted_Ca = fp8_multiply(184, weighted_Ca)\n' # -1
+                        + 'w_plast = clip(fp8_add(w_plast, weighted_Ca), 0, inf)\n')
 
-        self.on_post += 'w_plast = clip(w_plast + eta*Ca_pre, 0, inf)\n'
+        self.on_post += ('weighted_Ca = fp8_multiply(eta, Ca_pre)\n'
+                         + 'w_plast = clip(fp8_add(w_plast, weighted_Ca), 0, inf)\n')
 
         self.parameters = ParamDict({**self.parameters,
                                      **{'w_plast': 82}} # 10 in decimal
                                      )
         del self.parameters['weight']
+
+        self.namespace = ParamDict({**self.namespace,
+                                    **{'eta': 1}}) # Smallest learning rate
--- run_simulation.py
+++ run_simulation.py
@@ -2,6 +2,7 @@ from simulations.liquid_state_machine import liquid_state_machine
 from simulations.sleep_normalization import sleep_normalization
 from simulations.fp8_potjans_diesmann import fp8_potjans_diesmann
 from simulations.neuron_synapse_models import neuron_synapse_models
+from simulations.stdp import stdp
 from simulations.balanced_network import balanced_network
 
 import os
@@ -22,71 +23,110 @@ DEFAULT_FUNCTIONS.update({'stochastic_decay': stochastic_decay,
                           'fp8_smaller_than': fp8_smaller_than,
                           'deterministic_decay': deterministic_decay})
 
-parser = argparse.ArgumentParser(description=f'Main simulation file that '
-                                             f'calls files',
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--save_path', type=str,
+
+parser = argparse.ArgumentParser(
+    description='Main file that executes specified simulation',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--save_path',
+                    type=str,
                     default=f"""{datetime.now().strftime('%Y.%m.%d')}"""
                             f"""_{datetime.now().hour}."""
                             f"""{datetime.now().minute}/""",
                     help=f'directory to save, creating folder and overwriting '
                          f'existing ones. Defaults to datetime name.')
-parser.add_argument('--code_path', type=str, default='./code/',
+parser.add_argument('--code_path',
+                    type=str,
+                    default='./code/',
                     help='directory of compiled code')
-parser.add_argument('--quiet', action='store_true',
+parser.add_argument('--quiet',
+                    action='store_true',
                     help='whether to create plots/info or not')
-parser.add_argument('--backend', type=str, default='numpy',
+parser.add_argument('--backend',
+                    type=str,
+                    default='numpy',
                     help='Backend used by brian2 to run simulation')
-parser.add_argument('--timestep', default=1, type=float,
+parser.add_argument('--timestep',
+                    default=1,
+                    type=float,
                     help='timestep of the simulation (dt), in ms')
 
 subparsers = parser.add_subparsers(title='Simulation files available',
                                    help='Additional help for each available')
 
-subparser_lsm = subparsers.add_parser('LSM',
-                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-subparser_lsm.add_argument('--size', type=int, default=128,
+subparser_lsm = subparsers.add_parser(
+    'LSM',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+subparser_lsm.add_argument('--size',
+                           type=int,
+                           default=128,
                            help='size of the liquid')
-subparser_lsm.add_argument('--trial', type=int, default=0, help='trial number')
-subparser_lsm.add_argument('--precision', type=str, default='fp64',
+subparser_lsm.add_argument('--trial',
+                           type=int,
+                           default=0,
+                           help='trial number')
+subparser_lsm.add_argument('--precision',
+                           type=str,
+                           default='fp64',
                            help=f'Bit precision used. Currently only supports '
                                 f'8 and 64')
 subparser_lsm.set_defaults(func=liquid_state_machine)
 
-subparser_sleep = subparsers.add_parser('sleep',
-                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-subparser_sleep.add_argument('--size', type=int, default=128,
-                           help='size of the liquid')
-subparser_sleep.add_argument('--trial', type=int, default=0, help='trial number')
-subparser_sleep.add_argument('--precision', type=str, default='fp64',
-                           help=f'Bit precision used. Currently only supports '
-                                f'8 and 64')
+subparser_sleep = subparsers.add_parser(
+    'sleep',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+subparser_sleep.add_argument('--size',
+                             type=int,
+                             default=128,
+                             help='size of the liquid')
+subparser_sleep.add_argument('--trial',
+                             type=int,
+                             default=0,
+                             help='trial number')
+subparser_sleep.add_argument('--precision',
+                             type=str,
+                             default='fp64',
+                             help=f'Bit precision used. Currently only '
+                                  f'supports 8 and 64')
 subparser_sleep.set_defaults(func=sleep_normalization)
 
-subparser_pd = subparsers.add_parser('PD',
-                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-subparser_pd.add_argument('--protocol', type=int,
+subparser_pd = subparsers.add_parser(
+    'PD',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+subparser_pd.add_argument('--protocol',
+                          type=int,
                           help=f'Type of stimulation. 1 is for spontaneous '
                                f'input whereas 2 is for thalamic')
-subparser_pd.add_argument('--w_in', type=float,
+subparser_pd.add_argument('--w_in',
+                          type=float,
                           help=f'Relative strength of weights.')
-subparser_pd.add_argument('--bg_freq', type=float,
+subparser_pd.add_argument('--bg_freq',
+                          type=float,
                           help=f'Strength of background noise, in Hz.')
 subparser_pd.set_defaults(func=fp8_potjans_diesmann)
 
-subparser_models = subparsers.add_parser('models',
-                                         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+subparser_models = subparsers.add_parser(
+    'models',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 subparser_models.set_defaults(func=neuron_synapse_models)
 
-subparser_balance = subparsers.add_parser('balance',
-                                          formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-subparser_balance.add_argument('--w_perc', type=float,
+subparser_balance = subparsers.add_parser(
+    'balance',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+subparser_balance.add_argument('--w_perc',
+                               type=float,
                                help=f'Relative strength of weights. In the '
                                     f'case of minifloat, it represents '
                                     f'decimal value.')
-subparser_balance.add_argument('--trial', type=int, default=0,
+subparser_balance.add_argument('--trial',
+                               type=int,
+                               default=0,
                                help='trial number')
 subparser_balance.set_defaults(func=balanced_network)
+
+subparser_stdp = subparsers.add_parser(
+    'STDP',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+subparser_stdp.set_defaults(func=stdp)
 
 args = parser.parse_args()
 
