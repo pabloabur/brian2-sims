846be8c194b7a1d82a2782b689f43558828bbe89
pabloabur <pabloabur@gmail.com>
2023-07-18 12:22:43 +1000

    Adds better handling of model description
    The standard adopted was clashing with brian2 implementation, so I created a preprocessing stage that removes the problematic part. This should be fine because the original motivation for it was that customizing the models sometimes leads to unnoticed/undesired consequences. However, building the  model is well handled by brian2. One example of this was a synaptic pathway being added to model descript ('pre': 'spike') due to how a previous NeuronGroup that was built. Moreover, simply creating a new dict entry is also done, which is not possible with ParamDict.

--- core/builder/groups_builder.py
+++ core/builder/groups_builder.py
@@ -1,36 +1,60 @@
 import sys
 from warnings import warn
+import copy
 
 from brian2 import Synapses, NeuronGroup, ExplicitStateUpdater, get_device
 from brian2.units import *
 
+from core.equations.base_equation import ParamDict
+
 method = ExplicitStateUpdater('''x_new = f(x,t)''')
+
+def preprocess_model_description(model_description):
+    """
+    At the point where groups are actually created, the constraints
+    added previously are not helpful or clash with Brian2 processing. So
+    this function preprocess model description to remove instances of ParamDict
+    and prevent changes to affect dictionary structure.
+    """
+    desc = copy.copy(model_description)
+    attributes = [x for x in dir(desc) if not x.startswith('_')
+                                          and not callable(getattr(desc, x))]
+    for attr in attributes:
+        value = getattr(desc, attr)
+        if issubclass(ParamDict, type(value)):
+            setattr(desc, attr, dict(value))
+
+    return desc
 
 def create_synapses(source, target, model_desc, raise_warning=False,
                     name='synapses*'):
+    proc_model_desc = preprocess_model_description(model_desc)
     syn_group = Synapses(source, target,
-                         model=model_desc.model,
-                         on_pre=model_desc.on_pre,
-                         on_post=model_desc.on_post,
-                         namespace=model_desc.namespace,
+                         model=proc_model_desc.model,
+                         on_pre=proc_model_desc.on_pre,
+                         on_post=proc_model_desc.on_post,
+                         on_event=proc_model_desc.on_event,
+                         namespace=proc_model_desc.namespace,
                          name=name,
                          method=method)
-    syn_group.connect(**model_desc.connection)
-    set_params(syn_group, model_desc.parameters, raise_warning)
+    syn_group.connect(**proc_model_desc.connection)
+    set_params(syn_group, proc_model_desc.parameters, raise_warning)
 
     return syn_group
 
 def create_neurons(num_neurons, model_desc, raise_warning=False, 
                    name='neurongroup*'):
+    proc_model_desc = preprocess_model_description(model_desc)
     neu_group = NeuronGroup(num_neurons,
-                            model=model_desc.model,
-                            threshold=model_desc.threshold,
-                            reset=model_desc.reset,
-                            refractory=model_desc.refractory,
-                            namespace=model_desc.namespace,
+                            model=proc_model_desc.model,
+                            threshold=proc_model_desc.threshold,
+                            reset=proc_model_desc.reset,
+                            refractory=proc_model_desc.refractory,
+                            events=proc_model_desc.events,
+                            namespace=proc_model_desc.namespace,
                             name=name,
                             method=method)
-    set_params(neu_group, model_desc.parameters, raise_warning)
+    set_params(neu_group, proc_model_desc.parameters, raise_warning)
 
     return neu_group
 
