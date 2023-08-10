7651e374d6993bc55ed3cc4b0fa2d178ce43bc18
pabloabur <pabloabur@gmail.com>
2023-08-10 19:29:02 +1000

    Starts adding plotting for stdp

--- plots/stdp.R
+++ plots/stdp.R
@@ -1,11 +1,29 @@
 library(ggplot2)
 library(arrow)
 library(plotly)
+library(jsonlite)
+# TODO library(stringr)
 
 library(argparser)
 include('plots/parse_inputs.R')
 
+wd = getwd()
+data_path <- Sys.glob(file.path(wd, argv$source))
 
-a<-read_feather('sim_data/maoi/spikes.feather')
-b<-ggplot(a, aes(x=times, y=indices, color=type)) + geom_point() + theme_bw()
-# ggplotly(b)
+metadata <- fromJSON(file.path(dir, "metadata.json"))
+
+tsim <- as.double(str_sub(metadata$duration, 1, -2))
+
+#df_raster <- read.csv(data_path) %>%
+df_raster <- read.csv('output_spikes.csv')#%>%
+             #filter(time_ms>250 & time_ms<500)
+
+rates <- df_raster %>%
+  group_by(id) %>%
+  summarise(rate=n()/tsim)
+
+raster <- ggplot(df_raster, aes(x=time_ms, y=id)) +
+          geom_point(shape=20, size=1) + theme_bw()
+
+options("browser"="brave")
+ggplotly(raster)
