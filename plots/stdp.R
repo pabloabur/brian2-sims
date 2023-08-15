8c48e3964b1ee2311d8140181deb6426af752fb1
pabloabur <pabloabur@gmail.com>
2023-08-15 17:50:49 +1000

    Improves stdp plots

--- plots/stdp.R
+++ plots/stdp.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(plotly)
 library(jsonlite)
 library(stringr)
+library(purrr)
+library(patchwork)
 
 library(argparser)
 include('plots/parse_inputs.R')
@@ -14,6 +16,7 @@ metadata <- fromJSON(file.path(data_path, "metadata.json"))
 
 # Considered to be saved as second
 tsim <- as.double(str_sub(metadata$duration, 1, -2))
+N_exc <- metadata$N_exc
 
 df_raster <- read.csv(file.path(data_path, 'output_spikes.csv'))
 df_variables <- read.csv(file.path(data_path, 'output_vars.csv'))
@@ -40,17 +43,30 @@ freq_total <- df_rates_total %>%
         ggplot(aes(rate)) + theme_bw() +
         geom_histogram()
 
+# TODO color=id is wrong, look at filter id==1 or ggplotly
 ca_trace <- df_variables %>%
             filter(variable=='Ca') %>%
-            ggplot(aes(t_ms, val, color=id)) +
+            ggplot(aes(time_ms, value, color=id)) +
             geom_line()
+g_trace <- df_variables %>%
+           filter(variable=='g') %>%
+           ggplot(aes(time_ms, value, color=id)) +
+           geom_line()
 w_trace <- df_variables %>%
            filter(variable=='w_plast' & monitor=='stdp_in_w') %>%
-           ggplot(aes(t_ms, val, color=id)) +
+           ggplot(aes(time_ms, value, color=id)) +
            geom_line()
 
-raster <- ggplot(df_raster, aes(x=time_ms, y=id)) +
-          geom_point(shape=20, size=1) + theme_bw()
+# TODO #1: output from histogram over time, #2: same, ..., #N...;
+       # then they are summed together, resulting in same-length output
+       # then valvulate average, and mean for fano
+n_sample <- 1000
+hist_bins <- seq(0, tsim*1000, by=3)
+df_fano_factor <- df_raster %>%
+               filter(id %in% sample(N_exc, size=n_sample))
+var_binned_spikes = var(hist(df_fano_factor$time_ms, hist_bins, plot=F, right=F)$counts)
+mean_binned_spikes = mean(hist(df_fano_factor$time_ms, hist_bins, plot=F, right=F)$counts)
+fno = var_binned_spikes/mean_binned_spikes
 
 #options("browser"="brave")
 ggplotly(raster)
