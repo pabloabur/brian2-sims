b71f52de9edabf8a2178384b63f61b82689f8a45
Russell Jarvis <{ID}+{russelljjarvis}@users.noreply.github.com>
2023-08-24 15:02:52 +1000

    update code for pablo

--- test_song_bird.jl
+++ test_song_bird.jl
@@ -62,7 +62,7 @@ end
 
 function load_song_bird()
     spikes = Vector{Any}([])
-    nnn=Vector{UInt32}([])
+    nn=Vector{UInt32}([])
     ttt=Vector{Float32}([])
 
     file_read_list = readdlm("datasets/songbird_spikes.txt", '\t', Float64, '\n')
@@ -76,67 +76,126 @@ function load_song_bird()
     @inbounds for (i, t) in enumerate(spikes)
         for tt in t
             if length(t)!=0
-                push!(nnn,i);
+                push!(nn,i);
                 push!(ttt,Float32(tt))
             end
         end
     end
     numb_neurons=Int(maximum(nodes))+1
-
     maxt = (maximum(ttt))    
-    (nnn::Vector{UInt32},ttt::Vector{Float32},spikes::Vector{Any},numb_neurons::Integer,maxt::Real)
+    (nn::Vector{UInt32},ttt::Vector{Float32},spikes::Vector{Any},numb_neurons::Integer,maxt::Real)
 end
+
+function hist2dHeat(nodes::Vector{UInt32}, times::Vector{<:Real}, denom_for_bins::Real)
+    t0 = times
+    n0 = nodes
+    stimes = sort(times)
+    ns = maximum(unique(nodes))    
+    temp_vec = collect(0:Float64(maximum(stimes)/denom_for_bins):maximum(stimes))
+    templ = []
+    for (cnt,n) in enumerate(collect(1:maximum(nodes)+1))
+        push!(templ,[])
+    end
+    for (cnt,n) in enumerate(nodes)
+        push!(templ[n+1],times[cnt])    
+    end
+    list_of_artifact_rows = [] # These will be deleted as they bias analysis.
+    @inbounds @showprogress for (ind,t) in enumerate(templ)
+        psth = fit(Histogram,t,temp_vec)
+        if sum(psth.weights[:]) == 0.0
+            append!(list_of_artifact_rows,ind)
+        end
+    end
+    adjusted_length = ns+1-length(list_of_artifact_rows)
+    data = Matrix{Float64}(undef, adjusted_length, Int(length(temp_vec)-1))
+    cnt = 1
+    @inbounds @showprogress  for t in templ
+        psth = fit(Histogram,t,temp_vec)        
+        if sum(psth.weights[:]) != 0.0
+            data[cnt,:] = psth.weights[:]
+            @assert sum(data[cnt,:])!=0
+            cnt +=1
+        end
+    end
+    @inbounds for (ind,col) in enumerate(eachcol(data))
+        data[:,ind] .= (col.-mean(col))./std(col)
+    end
+    data[isnan.(data)] .= 0.0
+    #LinearAlgebra.normalize(data)
+    data::Matrix{Float64}
+end
+
 function initiatilize_data_pre_process()
     (nodes,times,spikes,numb_neurons,maxt) = load_song_bird()
+
     trains_array, neuron_ids = preprocess_spike_data(nodes, times)
 
+    @time psth = hist2dHeat(nodes,times,50.0)
     println("Calculating time-surface")
     exp_kernel = Vector{Float32}([exp(-x/30) for x in range(start=0, stop=999)])
     step_kernel = Vector{Float32}([0, 1, 0])
     sim_times = range(start=minimum(times), stop=maximum(times))
     time_surface = zeros(Float32, length(spikes), length(sim_times))        
     compute_liquid_states!(time_surface,trains_array, sim_times, exp_kernel, 1)
-    time_surface,trains_array,sim_times
+    time_surface,trains_array,sim_times,psth
 end
 
-@time time_surface,trains_array,sim_times = initiatilize_data_pre_process()
-function GetOnlinePCA(time_surface,trains_array,sim_times)
+
+@time time_surface,trains_array,sim_times,psth = initiatilize_data_pre_process()
+
+function GetOnlinePCA(time_surface,trains_array,sim_times,psth)
 
     println("Calculating UMAP")
     @time result_umap = umap(time_surface, 3)
     display(Plots.plot(scatter3d(result_umap[1, :],
               result_umap[2, :],
               result_umap[3, :],
-              zcolor=1:size(result_umap, 2), marker=(2, 2, :auto, stroke(0)))))
-    savefig("bird_song_UMAP.png")
+              zcolor=1:size(result_umap, 2))))
+    savefig("bird_song_UMAP3d.png")
+    display(Plots.plot(scatter(result_umap[1, :],result_umap[2, :],
+              zcolor=1:size(result_umap, 2))))
+    savefig("bird_song_UMAP2d.png")
+
     println("Calculating histograms and PCA")
-    psth = fit.(Histogram,
-                trains_array,
-                Ref(Vector{Float32}(sim_times[1]:2:sim_times[end])))
-    psth = [Vector{Float64}(x.weights) for x in psth]
-    psth = reduce(hcat, psth)'
+    
+    #hist2dHeat(nodes::Vector{UInt32}, times::Vector{Float32},
+    #psth = fit.(Histogram,
+    #            trains_array,
+    #            Ref(Vector{Float32}(sim_times[1]:2:sim_times[end])))
+    #psth = [Vector{Float64}(x.weights) for x in psth]
+    #psth = Matrix{Float32}(reduce(hcat, psth)')
     display(Plots.heatmap(psth))
+
     savefig("heatmap.png")
-    pca_dims = 6
-    #pca_interval = 1:length(psth)
-    pca_projections = CCIPCA(pca_dims, size(psth, 1))
-    #@showprogress for i in pca_interval
-    fit!(pca_projections, psth)
+    @time result_umap = umap(psth, 3)
+    display(Plots.plot(scatter3d(result_umap[1, :],
+              result_umap[2, :],
+              result_umap[3, :],
+              zcolor=1:size(result_umap, 2))))
+    savefig("bird_song_UMAP_PSTH_3d.png")
+    display(Plots.plot(scatter(result_umap[1, :],result_umap[2, :],
+              zcolor=1:size(result_umap, 2))))
+    savefig("bird_song_UMAP_PSTH_2d.png")
+
+
+    #pca_dims = 2
+    #pca_projections = CCIPCA(2,1480)
+    #psth_ = psth'[:]
+    #@show(typeof(psth_))
+    #fit!(pca_projections, reduce(hcat, psth)')
+
+    #psth_transformed = zeros(pca_dims, pca_interval[end])
+    #@showprogress for (i, col) in enumerate(eachcol(psth))
+    #    psth_transformed[:, i] = vec(OnlineStats.transform(pca_projections, col))
     #end
-    psth_transformed = zeros(pca_dims, pca_interval[end])
-    @showprogress for (i, col) in enumerate(eachcol(copy(psth[:, pca_interval])))
-        psth_transformed[:, i] = vec(OnlineStats.transform(pca_projections, col))
-    end
+    #OnlineStats.transform(pca_projections,psth)
+    #println("Variances explained by PCA")
+    #println(OnlineStats.relativevariances(pca_projections))
 
-    println("Variances explained by PCA")
-    println(OnlineStats.relativevariances(pca_projections))
-
-    println("Calculating UMAP with PCA")
-    @time result_umap2 = umap(psth_transformed, 3)
-    display(Plots.plot(scatter3d(result_umap2[1, :],
-              result_umap2[2, :],
-              result_umap2[3, :],
-              zcolor=1:size(result_umap2, 2), marker=(2, 2, :auto, stroke(0)))))
+    #println("Calculating UMAP with PCA")
+    #@time result_umap2 = umap(psth_transformed, 3)
+    #display(Plots.plot(scatter(psth_transformed[1, :],
+    #psth_transformed[2, :])))
     savefig("songbird_via_onlinePCA.png")
 end
-GetOnlinePCA(time_surface,trains_array,sim_times)
+GetOnlinePCA(time_surface,trains_array,sim_times,psth)
