f24275427efe24f55e2bba054b387d1665bc7c35
pabloabur <pabloabur@gmail.com>
2023-09-07 18:50:28 +1000

    Prepares scripts for plotting stdp

---
+++ plots/balance_stdp.R
@@ -0,0 +1,5 @@
+# TODO cover scenarios, e.g. NprerhoNpost, rho_a(t). Probaly merge all protocols, if memory permits
+#df_events <- read.csv(file.path(data_path, 'events_spike.csv'))
+#fig<-df_events  %>% ggplot(aes(x=time_ms, y=num_events)) + geom_line()
+#ggplotly(fig)
+
--- plots/stdp.R
+++ plots/stdp.R
@@ -1,10 +1,10 @@
-library(ggplot2)
-library(dplyr)
-library(plotly)
-library(jsonlite)
-library(stringr)
-library(purrr)
-library(patchwork)
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(plotly))
+suppressPackageStartupMessages(library(jsonlite))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(purrr))
+suppressPackageStartupMessages(library(patchwork))
 
 library(wesanderson)
 color_map <- wes_palette('GrandBudapest1')
@@ -12,11 +12,9 @@ color_map <- wes_palette('GrandBudapest1')
 library(argparser)
 include('plots/parse_inputs.R')
 
-# TODO Need to indicate this is a folder in path. This is the only plot from
-# this script
-#synv %>% filter(time_ms == 3599) %>% ggplot(aes(x=id, y=value)) + geom_line()
-
 wd = getwd()
+# TODO
+#argv$source <- 'bal_stdp_cudadgx3'
 data_path <- Sys.glob(file.path(wd, argv$source))
 
 metadata <- fromJSON(file.path(data_path, "metadata.json"))
@@ -27,13 +25,30 @@ N_exc <- metadata$N_exc
 
 df_raster <- read.csv(file.path(data_path, 'output_spikes.csv'))
 df_variables <- read.csv(file.path(data_path, 'output_vars.csv'))
-# TODO cover scenarios, e.g. NprerhoNpost, rho_a(t). Probaly merge all protocols, if memory permits
-#df_events <- read.csv(file.path(data_path, 'events_spike.csv'))
-#fig<-df_events  %>% ggplot(aes(x=time_ms, y=num_events)) + geom_line()
-#ggplotly(fig)
 
 # TODO
-#mean((df_raster %>% filter(time_ms>(tsim - 50)*1000 & time_ms<tsim*1000) %>% group_by(id) %>% summarise(mean_isi=mean(diff(time_ms)), sd_isi=sd(diff(time_ms))) %>% mutate(cv=sd_isi/mean_isi))$cv)
+#g_trace <- df_variables %>%
+#           filter(variable=='g') %>%
+#           ggplot(aes(x=time_ms, y=value, group=id, color=id)) +
+#           geom_line()
+#vm_trace <- df_variables %>%
+#           filter(variable=='Vm') %>%
+#           ggplot(aes(x=time_ms, y=value, group=id, color=id)) +
+#           geom_line()
+#options("browser"="brave")
+#ggplotly(vm_trace)
+#ggplotly(g_trace)
+
+df_base_stats <- df_raster %>%
+                 filter(id < N_exc+1) %>%
+                 group_by(id) %>%
+                 summarise(rate=n()/tsim,
+                           mean_isi=mean(diff(time_ms)),
+                           sd_isi=sd(diff(time_ms)))
+
+mean_rate = mean(df_base_stats$rate)
+print("Mean rate: ")
+print(mean_rate)
 
 print("Plotting rates")
 df_rates_init <- df_raster %>%
@@ -52,6 +67,10 @@ freq <- bind_rows(df_rates_init, df_rates_final) %>%
         geom_histogram(alpha=.3)
 ggsave(str_replace(argv$dest, '.png', '_freq.png'), freq)
 
+mean_cv <- mean(df_base_stats$sd_isi/df_base_stats$mean_isi, na.rm=T)
+print("Mean CV: ")
+print(mean_cv)
+
 print("Plotting traces")
 df_variables$id<-as.character(df_variables$id)
 ca_trace <- df_variables %>%
@@ -69,8 +88,20 @@ w_trace <- df_variables %>%
 traces <- ca_trace / g_trace / w_trace + plot_annotation(tag_levels = 'A')
 ggsave(str_replace(argv$dest, '.png', '_traces.png'), traces)
 
-print("Calculating Fano factor of last 50 seconds")
 n_sample <- 1000
+print("Calculating total Fano factor")
+df_fano_factor <- df_raster %>%
+                       filter(id %in% sample(N_exc, size=n_sample))
+hist_bins <- seq(min(df_fano_factor$time_ms),
+                 max(df_fano_factor$time_ms),
+                 by=3)
+hist_bins <- c(hist_bins, nth(hist_bins, -1) + 3)
+hist_counts = hist(df_fano_factor$time_ms, hist_bins, plot=F, right=F)$counts
+fno = var(hist_counts)/mean(hist_counts)
+print("Fano factor: ")
+print(fno)
+
+print("Calculating Fano factor of last 50 seconds")
 df_fano_factor_init <- df_raster %>%
                        filter(time_ms>0 & time_ms<50000) %>%
                        filter(id %in% sample(N_exc, size=n_sample))
@@ -82,6 +113,7 @@ hist_counts = hist(df_fano_factor_init$time_ms, hist_bins, plot=F, right=F)$coun
 fno = var(hist_counts)/mean(hist_counts)
 print("Fano factor in the first 50 seconds: ")
 print(fno)
+
 df_fano_factor_final <- df_raster %>%
                         filter(time_ms>(tsim - 50)*1000 & time_ms<tsim*1000) %>%
                         filter(id %in% sample(N_exc, size=n_sample))
