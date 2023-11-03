dd8200799d9c4c4d79826769173566da10ef16ee
pabloabur <pabloabur@gmail.com>
2023-11-03 19:28:26 +1100

    Starts drafting plotting script for large scale networks

--- plots/balance_stdp.R
+++ plots/balance_stdp.R
@@ -1,5 +1,127 @@
-# TODO cover scenarios, e.g. NprerhoNpost, rho_a(t). Probaly merge all protocols, if memory permits
-#df_events <- read.csv(file.path(data_path, 'events_spike.csv'))
-#fig<-df_events  %>% ggplot(aes(x=time_ms, y=num_events)) + geom_line()
-#ggplotly(fig)
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(arrow))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(plotly))
+suppressPackageStartupMessages(library(jsonlite))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(purrr))
+suppressPackageStartupMessages(library(patchwork))
 
+library(wesanderson)
+color_map <- wes_palette('GrandBudapest1')
+
+library(argparser)
+include('plots/parse_inputs.R')
+
+# TODO
+#argv$source<-'sim_data/ch3/balance_stdp/unimodal/'
+
+wd = getwd()
+data_path <- Sys.glob(file.path(wd, argv$source))
+
+metadata <- fromJSON(file.path(data_path, "metadata.json"))
+
+# Considered to be saved as second
+tsim <- as.double(str_sub(metadata$duration, 1, -2))
+N_exc <- metadata$N_exc
+
+# TODO remove, kept just in case
+#df_weights <- read_feather(file.path(data_path, 'obj_vars.feather'), as_data_frame=F)
+#write_parquet(df_weights, file.path(data_path, 'obj_vars.parquet'))
+#rm(df_weights)
+#gc()
+
+# TODO one from and to neuron
+#ds_weights <- open_dataset(file.path(data_path, 'obj_vars.feather'), format="arrow")
+#w_hist <- hist((ds_weights %>% select(w_plast) %>% collect())$w_plast, plot=F)
+#df_weights <- data.frame(count = w_hist$counts,
+#                         mids = w_hist$mids)
+#df_weights$mids <- df_weights$mids * 1000
+#
+#w_distribution <- df_weights %>%
+#    ggplot(aes(x=mids, y=count)) +
+#    geom_col(width=.019) + theme_bw() +
+#    labs(x='weights (mV)')
+## TODO color
+##    #scale_color_manual(values=color_map[2]) +
+##    #scale_fill_manual(values=color_map[2])
+#ggsave(str_replace(argv$dest, '.png', '_weight.png'), w_distribution)
+#gc()
+
+df_raster <- read.csv(file.path(data_path, 'output_spikes.csv'))
+df_base_stats <- df_raster %>%
+                 filter(id < N_exc+1) %>%
+                 group_by(id) %>%
+                 summarise(rate=n()/tsim,
+                           mean_isi=mean(diff(time_ms)),
+                           sd_isi=sd(diff(time_ms)))
+
+mean_rate = mean(df_base_stats$rate)
+print("Mean rate: ")
+print(mean_rate)
+
+mean_cv <- mean(df_base_stats$sd_isi/df_base_stats$mean_isi, na.rm=T)
+print("Mean CV: ")
+print(mean_cv)
+
+n_sample <- 1000
+print("Calculating total Fano factor")
+df_fano_factor <- df_raster %>%
+                       filter(id %in% sample(N_exc, size=n_sample))
+hist_bins <- seq(min(df_fano_factor$time_ms),
+                 max(df_fano_factor$time_ms),
+                 by=3)
+hist_bins <- c(hist_bins, nth(hist_bins, -1) + 3)
+hist_counts = hist(df_fano_factor$time_ms, hist_bins, plot=F, right=F)$counts
+fno = var(hist_counts)/mean(hist_counts)
+print("Fano factor: ")
+print(fno)
+
+print("Plotting rates")
+df_rates_init <- df_raster %>%
+                 filter(time_ms>0 & time_ms<2000) %>%
+                 group_by(id) %>%
+                 summarise(rate=n()/2)
+df_rates_final <- df_raster %>%
+                  filter(time_ms>(tsim - 2)*1000 & time_ms<tsim*1000) %>%
+                  group_by(id) %>%
+                  summarise(rate=n()/2)
+
+# TODO color
+df_rates_final$group <- 'final'
+df_rates_init$group <- 'initial'
+freq <- bind_rows(df_rates_init, df_rates_final) %>%
+        ggplot(aes(x=rate, group=group, color=group, fill=group)) + theme_bw() +
+        geom_histogram(alpha=.3)
+ggsave(str_replace(argv$dest, '.png', '_freq.png'), freq)
+
+# TODO raster?
+raster_init <- df_raster %>% filter(time_ms>=0*1000 & time_ms<=5*1000) %>% ggplot(aes(x=time_ms, y=id)) + geom_point(shape=20, size=.1, alpha=.01) + theme_bw()
+raster_final <- df_raster %>% filter(time_ms>=190*1000 & time_ms<=195*1000) %>% ggplot(aes(x=time_ms, y=id)) + geom_point(shape=20, size=.1, alpha=.01) + theme_bw()
+raster <- raster_init / raster_final
+ggsave(str_replace(argv$dest, '.png', '_raster.png'), raster)
+
+print("Calculating Fano factor of last 50 seconds")
+df_fano_factor_init <- df_raster %>%
+                       filter(time_ms>0 & time_ms<50000) %>%
+                       filter(id %in% sample(N_exc, size=n_sample))
+hist_bins <- seq(min(df_fano_factor_init$time_ms),
+                 max(df_fano_factor_init$time_ms),
+                 by=3)
+hist_bins <- c(hist_bins, nth(hist_bins, -1) + 3)
+hist_counts = hist(df_fano_factor_init$time_ms, hist_bins, plot=F, right=F)$counts
+fno = var(hist_counts)/mean(hist_counts)
+print("Fano factor in the first 50 seconds: ")
+print(fno)
+
+df_fano_factor_final <- df_raster %>%
+                        filter(time_ms>(tsim - 50)*1000 & time_ms<tsim*1000) %>%
+                        filter(id %in% sample(N_exc, size=n_sample))
+hist_bins <- seq(min(df_fano_factor_final$time_ms),
+                 max(df_fano_factor_final$time_ms),
+                 by=3)
+hist_bins <- c(hist_bins, nth(hist_bins, -1) + 3)
+hist_counts = hist(df_fano_factor_final$time_ms, hist_bins, plot=F, right=F)$counts
+fno = var(hist_counts)/mean(hist_counts)
+print("Fano factor in the last 50 seconds: ")
+print(fno)
--- scripts/R.sh
+++ scripts/R.sh
@@ -1,1 +1,1 @@
-Rscript --vanilla plots/multi_fetch_stdp.R -s sim_data/ch3/multi_stdp_fetches/ -d ~/git/phd_thesis/figs/fetches_stdp2.png
+Rscript --vanilla plots/balance_stdp.R -s sim_data/ch3/balance_stdp/unimodal #-d ~/git/phd_thesis/figs/fetches_stdp2.png
