e293f22a2c2ba2ef0dcc3c1fc9fbe745aed22b04
pabloabur <pabloabur@gmail.com>
2024-01-18 14:01:52 +1100

    Adds code that handles STDP with stochastic rounding

--- scripts/R.sh
+++ scripts/R.sh
@@ -1,2 +1,3 @@
-#Rscript --vanilla plots/fp8_simple_stdp.R -s sim_data/ch4/simple_stdp/ #-d ~/git/phd_thesis/figs/balance_stdp.png
-Rscript --vanilla plots/stochastic_operations.R -s sim_data/ch5/sr/add -d ~/git/phd_thesis/figs/stoch_round_probs.png
+#Rscript --vanilla plots/fp8_simple_stdp.R -s sim_data/ch4/simple_stdp/ -d ~/git/phd_thesis/figs/stoch_simple_stdp.png
+#Rscript --vanilla plots/stochastic_operations.R -s sim_data/ch4/sr/add -d ~/git/phd_thesis/figs/stoch_round_probs.png
+Rscript --vanilla plots/stochastic_kernel.R -s sim_data/ch4/kernel/ -d ~/git/phd_thesis/figs/stoch_kernel.png
